666ec1e748bc965373e299cc1eb65c16293ff160
Michael Kinnas <michaelkinnas@gmail.com>
2023-10-09 17:24:17 +0300

    refactored Frugality class

--- metrics_oop.py
+++ metrics_oop.py
@@ -12,34 +12,29 @@ class Frugality():
     """ 
     Calculates frugality scores of neural network algorithms given prediction accuracy and resource consumption.
     """
-    def __init__(self, values: Iterable[tuple[str, float, float]] = None):
-        self.x_points = np.linspace(0, 1, num=2)
+    def __init__(self):
+        self.records = [] # list of tuples
 
-        self.values = [values] # list of tuples
-        self.ys = [] #list of lists of calculated y points for each tuple
-        self.labels = []
 
-        if values != None:
-            self.add_many(values)
-       
-        
     def calculate(self, P_aj: float, R_aj: float, w: float) -> float:        
         return P_aj - (w / (1 + (1 / R_aj)))
     
     
-    def calculate_y_points(self, accuracy: float, resource: float) -> list[float]:
+    def __calculate_y_points(self, accuracy: float, resource: float) -> list[float]:
+        x_points = np.linspace(0, 1, num=2)
+
         if accuracy < 0 or accuracy > 1:
             raise ValueError('Accuracy values must be between 0 and 1')
 
         if resource <= 0:
             raise ValueError('Resource values must be greater than 0')
 
-        return [self.calculate(accuracy, resource, x) for x in self.x_points]
+        return [[self.calculate(accuracy, resource, x) for x in x_points], list(x_points)]
     
     
-    def add(self, name: str, accuracy: float, resources: float):
-        self.ys.append(self.calculate_y_points(accuracy, resources))
-        self.labels.append(name)
+    def add(self, label: str, accuracy: float, resources: float):
+        x, y = self.__calculate_y_points(accuracy, resources)      
+        self.records.append((label, accuracy, resources, [x, y]))
     
     
     def add_many(self, values: Iterable[tuple[str, float, float]]):
@@ -56,8 +51,20 @@ class Frugality():
         plt.xlim([0, 1])
         plt.ylim([0, 1])
 
-        for score, name in zip(self.ys, self.labels):
-            plt.plot(self.x_points, score, label=name)
+        for label, (x, y) in [a[0:4:3] for a in self.records]:
+            plt.plot(x, y, label=label)
 
         plt.legend()
         plt.show()
+
+
+    def values(self):
+        for item in self.records:
+            label, acc, res = item[:3]
+            print(f'label: {label}, accuracy: {acc}, resources: {res}')
+
+    
+frug = Frugality()
+frug.add_many([('aa', 0.50, 500), ('bb', 0.98, 600)])
+frug.plot()
+frug.values()
